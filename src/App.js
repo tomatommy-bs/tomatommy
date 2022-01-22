9de3ca92155991cd5dfb809c4afb6d080eea6513
oura-hideyoshi <217x004x@stu.kobe-u.ac.jp>
2022-01-22 20:37:34 +0900

    github-pages 適用

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   console.log('process.env.PUBLIC_URL', process.env.PUBLIC_URL);
@@ -17,7 +17,12 @@ function App() {
 }
 
 function Home() {
-  return <h2>Home</h2>;
+  return (
+  <>
+    <h2>Home</h2>
+    <Link to={"/about"}>about</Link>
+  </>
+  );
 }
 
 function About() {
